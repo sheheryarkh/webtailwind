1c6fd14b95dc609977428ed776ddc865091bb52c
Sheheryar khan <154220737+sheheryarkh@users.noreply.github.com>
2024-11-10 12:19:42 +0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <div>
-      <header className="flex gap-3 py-4 px-2 bg-yellow-100">
+      <header className="flex gap-3 py-4 px-2 bg-yellow-100 max-sm:text-white">
         <div className="ml-4">khanz web</div>
         <nav className="flex list-none gap-4 ml-auto mr-2 ">
           <Link className=" hover:text-sm " href="/"><li>home</li></Link>
