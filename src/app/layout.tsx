85854d208fb5cadf12681c00c08c79f62e9588dd
Sheheryar khan <154220737+sheheryarkh@users.noreply.github.com>
2024-11-10 12:11:53 +0500

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+{/*       <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta> */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
