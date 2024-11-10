187e8426582c654dad5ce23ec234ac015524a192
Sheheryar khan <154220737+sheheryarkh@users.noreply.github.com>
2024-11-10 06:40:02 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import web from "@/app/pics/dev.png"
 export default function Home() {
   return (
     <>
-    <main className="flex mx-9 justify-evenly gap max-sm:flex-col mt-4 bg-black text-white">
+    <main className="flex mx-9 justify-evenly gap max-sm:flex-col mt-4">
       <div className="mt-5"><h1>A full-stack developer is a software developer who is proficient in both front-end (client-side) and back-end (server-side) technologies. They work on the entire stack of a web application, from the user interface to the database and server logic. Full-stack developers often handle everything from building the user-facing parts to setting up and maintaining databases, APIs, and server infrastructure.</h1></div>
       <div className=" md: mt-6 mx-auto"><Image width={900} src={web} alt="picture"></Image></div>
     </main>
